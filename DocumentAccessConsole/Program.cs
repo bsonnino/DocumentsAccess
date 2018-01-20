55410bbe66c346a56fdc6312c0c64952b08617a0
Bruno Sonnino <sonnino@revolution.com.br>
2018-01-20 10:11:25 -0200

    Add project files.

---
+++ DocumentAccessConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DocumentAccessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var docsLibrary = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var files = Directory.GetFiles(docsLibrary, "*.*", SearchOption.AllDirectories);
+            var fileInfos = files.Select(f => new FileInfo(f)).ToList();
+            ValueType totalSize = fileInfos.Sum(f => f.Length);
+            sw.Stop();
+            Console.WriteLine($"Ellapsed time: {sw.ElapsedMilliseconds}  {fileInfos.Count} files - Total Size {totalSize}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DocumentsAccess/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DocumentsAccess
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void GetFolder(object sender, RoutedEventArgs e)
+        {
+            var folder = await SelectFolderAsync();
+            if (folder != null)
+            {
+                DocsGrid.ItemsSource = null;
+                StatusTxt.Text = "";
+                var sw = new Stopwatch();
+                sw.Start();
+                var queryOptions = new QueryOptions
+                {
+                    FolderDepth = FolderDepth.Deep,
+                    IndexerOption = IndexerOption.UseIndexerWhenAvailable
+                };
+                var query = folder.CreateFileQueryWithOptions(queryOptions);
+                var allFiles = await query.GetFilesAsync();
+                DocsGrid.ItemsSource = allFiles;
+                sw.Stop();
+                StatusTxt.Text = $"Ellapsed time: {sw.ElapsedMilliseconds}  {allFiles.Count} files";
+            }
+        }
+
+        private static async Task<StorageFolder> SelectFolderAsync()
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker
+            {
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop
+            };
+            folderPicker.FileTypeFilter.Add("*");
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            return folder;
+        }
+
+        private async void GetFolderSlow(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await SelectFolderAsync();
+            if (folder != null)
+            {
+                DocsGrid.ItemsSource = null;
+                StatusTxt.Text = "";
+                var sw = new Stopwatch();
+                sw.Start();
+                _allFiles = new List<StorageFile>();
+                await GetFilesInFolder(folder);
+                DocsGrid.ItemsSource = _allFiles;
+                sw.Stop();
+                StatusTxt.Text = $"Ellapsed time: {sw.ElapsedMilliseconds}  {_allFiles.Count} files";
+            }
+        }
+
+        private List<StorageFile> _allFiles;
+
+        private async Task GetFilesInFolder(StorageFolder folder)
+        {
+            var items = await folder.GetItemsAsync();
+            foreach (var item in items)
+            {
+                if (item is StorageFile)
+                    _allFiles.Add(item as StorageFile);
+                else
+                    await GetFilesInFolder(item as StorageFolder);
+            }
+        }
+    }
+}
